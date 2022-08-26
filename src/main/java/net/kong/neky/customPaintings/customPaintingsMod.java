330216b27cb536be1f8a13e1d87d3e14f1f04635
Neky <germarten@gmail.com>
2022-08-27 01:10:20 +0200

    Downgraded Version from 1.19 to 1.18.2 + initial work

---
+++ src/main/java/net/kong/neky/customPaintings/customPaintingsMod.java
@@ -0,0 +1,17 @@
+package net.kong.neky.customPaintings;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class customPaintingsMod implements ModInitializer {
+
+	public static final String MOD_ID = "custompaintings";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
