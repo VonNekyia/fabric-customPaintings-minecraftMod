37e20700cb3a522927081cdb97aee2098567ed13
Kubi <juliankubitza04@gmail.com>
2022-08-27 15:46:24 +0200

    added textures folder

--- src/main/java/net/kong/neky/customPaintings/painting/ModPainting.java
+++ src/main/java/net/kong/neky/customPaintings/painting/ModPainting.java
@@ -6,7 +6,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModPainting {
-    public static final PaintingMotive TEST = registerPainting("test", new PaintingMotive(16, 16));
+
+    static int block = 16;
+
+    public static final PaintingMotive AZADI_TOWER = registerPainting("azadi_tower", new PaintingMotive(block * 3, block * 2));
+    public static final PaintingMotive KUALA_LUMPER = registerPainting("kuala_lumper", new PaintingMotive(block * 3, block * 2));
+    public static final PaintingMotive OBSERVATION_DECK = registerPainting("observation_deck", new PaintingMotive(block * 3, block * 2));
+    public static final PaintingMotive TOY_PIKACHU = registerPainting("toy_pikachu", new PaintingMotive(block * 3, block * 2));
+
+
 
     private static PaintingMotive registerPainting(String name, PaintingMotive paintingMotive) {
         return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(customPaintingsMod.MOD_ID, name), paintingMotive);
